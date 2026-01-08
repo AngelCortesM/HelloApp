189d455e1df7cc9b9a9f7544a2bb4feb39dcbaa5
AngelCortesM <joseangelcortesmontoya@gmail.com>
2026-01-08 14:19:47 -0500

    Agregar método Conversions para demostrar conversiones de tipos; actualizar artefactos de compilación

--- 02-Logic/Conversions.cs
+++ 02-Logic/Conversions.cs
@@ -1,11 +1,30 @@
 using static System.Console;
+using static System.Math;
 partial class Program
 {
     static void Conversions()
     {
-        //Conversiones implícitas
+        // Conversiones implícitas
         int number = 42;
         double decimalNumber = number; // int a double
-        WriteLine($"Número entero: {number}, Número decimal: {decimalNumber}"); 
+        WriteLine($"Número entero: {number}, Número decimal: {decimalNumber}");
+
+        double explicitDecimalNumber = 42.6;
+        int integerNumber = (int)explicitDecimalNumber;
+        WriteLine($"Número decimal: {explicitDecimalNumber}, Número entero (después de conversión explícita): {integerNumber}");
+
+        //Convert o Parse
+        // Parse
+        string text = "12345";
+        int parsedNumber = int.Parse(text);
+        WriteLine($"Texto: {text}, Número parseado: {parsedNumber}");
+
+        // Convert
+        double anotherDecimalNumber = 67.89;
+        int parsedFloat = Convert.ToInt32(anotherDecimalNumber);
+        WriteLine($"Texto: {anotherDecimalNumber}, Número decimal parseado: {parsedFloat}");//redondea
+        int castedNumber = (int)anotherDecimalNumber;
+        WriteLine($"Texto: {anotherDecimalNumber}, Número decimal casteado: {castedNumber}");//trunca
     }
+
 }
--- obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae7560521b66e769c4028d5bcbfcd911ea2b8a5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf5709ffd3ce1200d163d2315b217ad1bf2f0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
