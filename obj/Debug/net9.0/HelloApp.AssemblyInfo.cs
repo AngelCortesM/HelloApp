7861080df4b5e20583b50a0b4793813be064f067
AngelCortesM <joseangelcortesmontoya@gmail.com>
2026-01-13 08:25:51 -0500

    Agregar instrucciones de seguridad de Snyk para mejores prácticas en el proyecto

--- obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189d455e1df7cc9b9a9f7544a2bb4feb39dcbaa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+007a14ca80032ab24859e8d119f39574ba86c8fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
